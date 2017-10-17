4cb4ff755f7fc2da0405b16e777ce538bb3df4bc
Adrian Stainforth <adrian.stainforth@gmail.com>
2017-10-17 20:12:05 +0100

    FIXING ISSUES

--- src/components/sessions/stateless/expand_btn.jsx
+++ src/components/sessions/stateless/expand_btn.jsx
@@ -1,5 +1,22 @@
 import React from 'react';
 
+const circleStyles = {
+  'stroke': '#e14e00'
+  , 'stroke-width': '2px'
+  , 'fill': 'transparent'
+};
+
+const lineStyles = {
+  'stroke': '#e14e00'
+  , 'stroke-width': '2px'
+};
+
+const ellipseStyles = {
+  "fill": '#e14e00'
+  , 'stroke': '#e14e00'
+  , 'stroke-width': '2px'
+};
+
 const ExpandBtn = function(props){
   return (
       <div className={props.css}>
@@ -9,9 +26,9 @@ const ExpandBtn = function(props){
           title={props.text} >
           <svg id="icons" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 42 42" width="35" height="35">
             <title>{props.text}</title>
-            <circle style={info_styles} cx="21" cy="21" r="20"/>
-            <line style={info_styles} x1="21" y1="17.4" x2="21" y2="31.2"/>
-            <ellipse style={info_styles} cx="21" cy="11.2" rx="0.6" ry="0.4"/>
+            <circle style={circleStyles} cx="21" cy="21" r="20"/>
+            <line style={lineStyles} x1="21" y1="17.4" x2="21" y2="31.2"/>
+            <ellipse style={ellipseStyles} cx="21" cy="11.2" rx="0.6" ry="0.4"/>
           </svg>
         </a>
       </div>
